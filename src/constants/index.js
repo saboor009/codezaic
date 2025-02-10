e1c6977e1c63dc53775ecd84659a1347c513bbfe
Booray <150834732+saboor009@users.noreply.github.com>
2025-02-11 00:40:34 +0500

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,303 @@
+import {
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  chromecast,
+  disc02,
+  discord,
+  discordBlack,
+  facebook,
+  figma,
+  file02,
+  framer,
+  homeSmile,
+  instagram,
+  notification2,
+  notification3,
+  notification4,
+  notion,
+  photoshop,
+  plusSquare,
+  protopie,
+  raindrop,
+  recording01,
+  recording03,
+  roadmap1,
+  roadmap2,
+  roadmap3,
+  roadmap4,
+  searchMd,
+  slack,
+  sliders04,
+  telegram,
+  twitter,
+  yourlogo,
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  photo2
+  
+} from "../assets";
+
+export const navigation = [
+  {
+    id: "0",
+    title: "Portfolio",
+    url: "https://www.instagram.com/codezaic",
+  },
+
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+
+export const brainwaveServices = [
+  "Prototyping",
+  "Wireframing",
+  "Landing Page",
+];
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+export const roadmap = [
+  {
+    id: "0",
+    title: "Voice recognition",
+    text: "Enable the chatbot to understand and respond to voice commands, making it easier for users to interact with the app hands-free.",
+    date: "May 2023",
+    status: "done",
+    imageUrl: roadmap1,
+    colorful: true,
+  },
+  {
+    id: "1",
+    title: "Gamification",
+    text: "Add game-like elements, such as badges or leaderboards, to incentivize users to engage with the chatbot more frequently.",
+    date: "May 2023",
+    status: "progress",
+    imageUrl: roadmap2,
+  },
+  {
+    id: "2",
+    title: "Chatbot customization",
+    text: "Allow users to customize the chatbot's appearance and behavior, making it more engaging and fun to interact with.",
+    date: "May 2023",
+    status: "done",
+    imageUrl: roadmap3,
+  },
+  {
+    id: "3",
+    title: "Integration with APIs",
+    text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
+    date: "May 2023",
+    status: "progress",
+    imageUrl: roadmap4,
+  },
+];
+
+export const collabText =
+  "Get tailor-made solutions to address your unique business needs and challenges.";
+
+export const collabContent = [
+  {
+    id: "0",
+    title: "Scalable Enterprise Solutionsn",
+    text: "Grow your business with our digital solutions.",
+  },
+  {
+    id: "1",
+    title: "Cross-platform Compatibility",
+  },
+  {
+    id: "2",
+    title: "Custom Development",
+  },
+  {
+    id: "3",
+    title: "Advanced Security",
+  },
+];
+
+export const collabApps = [
+  {
+    id: "0",
+    title: "Figma",
+    icon: figma,
+    width: 26,
+    height: 36,
+  },
+  {
+    id: "1",
+    title: "Notion",
+    icon: notion,
+    width: 34,
+    height: 36,
+  },
+  {
+    id: "2",
+    title: "Discord",
+    icon: discord,
+    width: 36,
+    height: 28,
+  },
+  {
+    id: "3",
+    title: "Slack",
+    icon: slack,
+    width: 34,
+    height: 35,
+  },
+  {
+    id: "4",
+    title: "Photoshop",
+    icon: photoshop,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "5",
+    title: "Protopie",
+    icon: protopie,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "6",
+    title: "Framer",
+    icon: framer,
+    width: 26,
+    height: 34,
+  },
+  {
+    id: "7",
+    title: "Raindrop",
+    icon: raindrop,
+    width: 38,
+    height: 32,
+  },
+];
+
+export const pricing = [
+  {
+    id: "0",
+    title: "Basic",
+    description: "AI chatbot, personalized recommendations",
+    price: "0",
+    features: [
+      "An AI chatbot that can understand your queries",
+      "Personalized recommendations based on your preferences",
+      "Ability to explore the app and its features without any cost",
+    ],
+  },
+  {
+    id: "1",
+    title: "Premium",
+    description: "Advanced AI chatbot, priority support, analytics dashboard",
+    price: "9.99",
+    features: [
+      "An advanced AI chatbot that can understand complex queries",
+      "An analytics dashboard to track your conversations",
+      "Priority support to solve issues quickly",
+    ],
+  },
+  {
+    id: "2",
+    title: "Enterprise",
+    description: "Custom AI chatbot, advanced analytics, dedicated account",
+    price: null,
+    features: [
+      "An AI chatbot that can understand your queries",
+      "Personalized recommendations based on your preferences",
+      "Ability to explore the app and its features without any cost",
+    ],
+  },
+];
+
+export const benefits = [
+  {
+    id: "0",
+    title: "Custom Designs",
+    text: "Tailored web designs to fit your business needs perfectly.",
+    backgroundUrl: "./src/assets/benefits/card-1.svg",
+    iconUrl: one,
+ 
+  },
+  {
+    id: "1",
+    title: "Quick Delivery",
+    text: "Fast and Reliable Delivery to Bring Your Ideas to Life on Time.",
+    backgroundUrl: "./src/assets/benefits/card-2.svg",
+    iconUrl: two,
+  },
+  {
+    id: "2",
+    title: "Expert Team",
+    text: "Driven by an Expert Team Committed to Excellence and Innovation.",
+    backgroundUrl: "./src/assets/benefits/card-3.svg",
+    iconUrl:three,
+  },
+  {
+    id: "3",
+    title: "Ongoing Support",
+    text: "Continuous Support to Keep Your Business Running Smoothly.",
+    backgroundUrl: "./src/assets/benefits/card-4.svg",
+    iconUrl: four,  
+  },
+  {
+    id: "4",
+    title: "Performance Optimization",
+    text: "Lightning-Fast Performance for a Smooth User Experience.",
+    backgroundUrl: "./src/assets/benefits/card-5.svg",
+    iconUrl: five,
+
+  },
+  {
+    id: "5",
+    title: "User Friendly",
+    text: "Intuitive and User-Friendly Designs for Effortless Navigation.",
+    backgroundUrl: "./src/assets/benefits/card-6.svg",
+    iconUrl: six,
+  },
+];
+
+export const socials = [
+ 
+  {
+    id: "1",
+    title: "Twitter",
+    iconUrl: twitter,
+    url: "https://www.instagram.com/codezaic",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: instagram,
+    url: "https://www.instagram.com/codezaic",
+  },
+  {
+    id: "3",
+    title: "Telegram",
+    iconUrl: telegram,
+    url: "https://www.instagram.com/codezaic",
+  },
+  {
+    id: "4",
+    title: "Facebook",
+    iconUrl: facebook,
+    url: "https://www.instagram.com/codezaic",
+  },
+];
